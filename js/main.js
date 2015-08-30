71547e5af7f4fe6ab71244877e309c463b7bee32
Eachan Johnson <ejohnson@broadinstitute.org>
2015-08-30 14:36:59 -0400

    Basic functionality: fonts, title slide, fonts, single-column text-only layout, keyboard slide transitions, black-out

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Slype main JavaScript file
+function main () {
+    slideTransition();
+} 
+
+$(document).ready(function () {
+    main();
+});
---
+++ js/slide-handling.js
@@ -0,0 +1,84 @@
+// Slype handler for slide transitions
+
+function goToNextSlide () {
+    var $previousSlide = $('.previous-slide'),
+        $currentSlide = $('.current-slide'),
+        $nextSlide = $('.next-slide'),
+        $nextNextSlide = $nextSlide.next('.slide');
+    
+    if ( $nextSlide.length > 0 ) {
+        console.log( 'Moving forward one slide.' );
+        $previousSlide.removeClass('previous-slide');
+        $currentSlide.addClass('previous-slide');
+        $currentSlide.removeClass('current-slide');
+        $nextSlide.removeClass('next-slide');
+        $nextSlide.addClass('current-slide');
+        $nextNextSlide.addClass('next-slide');
+        return( $nextSlide )
+    } else {
+        console.log( 'Already on the last slide.' );
+        return( $currentSlide )
+    }
+}
+
+function goToPreviousSlide () {
+    var $previousSlide = $('.previous-slide'),
+        $currentSlide = $('.current-slide'),
+        $nextSlide = $('.next-slide'),
+        $previousPreviousSlide = $previousSlide.prev('.slide');
+    
+    if ( $previousSlide.length > 0 ) {
+        console.log( 'Moving back one slide.' );
+        $nextSlide.removeClass('next-slide');
+        $currentSlide.addClass('next-slide');
+        $currentSlide.removeClass('current-slide');
+        $previousSlide.removeClass('previous-slide');
+        $previousSlide.addClass('current-slide');
+        $previousPreviousSlide.addClass('previous-slide');
+        return( $previousSlide )
+    } else {
+        console.log( 'Already on the first slide.' );
+        return( $currentSlide )
+    }
+}
+
+function toggleDark () {
+    var $slides = $('.slide'),
+        $body = $('body');
+    
+    $slides.toggle();
+    $body.toggleClass('black-out');
+};
+
+function slideTransition () {
+    // Listen for keyboard and act apprpriately
+    var $body = $('body');
+    
+    $body.keyup(function ( event ) {
+        var keyPressed = String.fromCharCode( event.which );
+        console.log( 'User pressed ' + keyPressed );
+        
+        switch (keyPressed) {
+            case ' ':
+                goToNextSlide();
+                break;
+            case '&':  // up arrow
+                goToPreviousSlide();
+                break;
+            case '(':  // down arrow
+                goToNextSlide();
+                break;
+            case '%':  // left arrow
+                goToPreviousSlide();
+                break;
+            case '\'':  // right arrow
+                goToNextSlide();
+                break;
+            case 'B':  // right arrow
+                toggleDark();
+                break;
+            default:
+                console.log( 'Key pressed, but not taking any transition action' );
+       }
+    });
+}
